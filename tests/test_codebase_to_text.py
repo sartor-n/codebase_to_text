8d2908cace8081dcaec0c943969261f1cc4dc295
Nicholas Sartor <nsartor@mindthebridge.org>
2024-08-27 11:09:48 +0200

    Updated tests

--- tests/test_codebase_to_text.py
+++ tests/test_codebase_to_text.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import sys
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from codebase_to_text.codebase_to_text import CodebaseToText
 import shutil
@@ -11,21 +12,60 @@ class TestCodebaseToText(unittest.TestCase):
         # Create a temporary folder with some test files
         self.test_folder_path = "test_folder"
         os.makedirs(self.test_folder_path, exist_ok=True)
-        with open(os.path.join(self.test_folder_path, "test_file1.txt"), "w") as file:
+        with open(os.path.join(self.test_folder_path, "test_file1.py"), "w") as file:
             file.write("Test file 1 content")
         with open(os.path.join(self.test_folder_path, "test_file2.txt"), "w") as file:
             file.write("Test file 2 content")
+        with open(os.path.join(self.test_folder_path, "test_file3.csv"), "w") as file:
+            file.write("Test file 3 content")
+        self.maxDiff = None
 
     def test_get_text(self):
-        code_to_text = CodebaseToText(input_path=self.test_folder_path, output_path="output.txt", output_type="txt")
+        code_to_text = CodebaseToText(
+            input_path=self.test_folder_path,
+            output_path="output.txt",
+            output_type="txt",
+            file_extensions=[".txt", ".py"],
+        )
+
         text = code_to_text.get_text()
-        expected_text = f"Folder structure:\n{self.test_folder_path}/\n    test_file1.txt\n    test_file2.txt\n\nFile Contents:\n\n{self.test_folder_path}/test_file1.txt\nFile type: Text (.txt)\nTest file 1 content\n\n{self.test_folder_path}/test_file2.txt\nFile type: Text (.txt)\nTest file 2 content"
+        print(text)
+        expected_text = f"""Folder Structure
+--------------------------------------------------
+{self.test_folder_path}/
+    test_file1.py
+    test_file2.txt
+    test_file3.csv
+
+
+File Contents
+--------------------------------------------------
+
+
+{self.test_folder_path}/test_file1.py
+File type: .py
+Test file 1 content
+
+--------------------------------------------------
+File End
+--------------------------------------------------
+
+
+{self.test_folder_path}/test_file2.txt
+File type: .txt
+Test file 2 content
+
+--------------------------------------------------
+File End
+--------------------------------------------------
+"""
         self.assertEqual(text, expected_text)
 
     def tearDown(self):
         # Clean up temporary folder
         if os.path.exists(self.test_folder_path):
             shutil.rmtree(self.test_folder_path)
+
 
 if __name__ == "__main__":
     print(sys.path)
